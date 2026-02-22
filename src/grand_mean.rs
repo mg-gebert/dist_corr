042e06773915daa36480c188453191accfcf4d5f
Philipp Gesang <phg@phi-gamma.net>
2026-02-22 21:37:20 +0100

    grand_mean: remove redundant field
    The wrapped reference is a fat pointer that already contains the
    length of the pointed-to slice.

--- src/grand_mean.rs
+++ src/grand_mean.rs
@@ -8,7 +8,6 @@ use itertools::izip;
 
 pub struct GrandMeans<'a> {
     v: &'a [f64],
-    len: usize,
 }
 
 // +++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -18,9 +17,12 @@ pub struct GrandMeans<'a> {
 impl<'a> GrandMeans<'a> {
     /// Create a new GrandMeans provider instance.
     pub fn new(v: &'a [f64]) -> Self {
-        let len = v.len();
+        Self { v }
+    }
 
-        Self { v, len }
+    #[inline]
+    fn len(&self) -> usize {
+        self.v.len()
     }
 
     /// Computes the grand means of the matrix
@@ -42,19 +44,19 @@ impl<'a> GrandMeans<'a> {
     pub fn compute_ordered(self) -> Vec<f64> {
         let mut current_sum_ascending = 0.0;
         let mut current_sum_descending = 0.0;
-        let mut out = vec![0.0; self.len];
+        let mut out = vec![0.0; self.len()];
 
         izip!(self.v.iter(), self.v.iter().rev())
             .enumerate()
             .for_each(|(i, (v_i, rev_v_i))| {
                 out[i] +=
-                    (2 * (i as i64) - self.len as i64 + 1) as f64 * v_i - current_sum_ascending;
-                out[self.len - i - 1] += current_sum_descending;
+                    (2 * (i as i64) - self.len() as i64 + 1) as f64 * v_i - current_sum_ascending;
+                out[self.len() - i - 1] += current_sum_descending;
                 current_sum_descending += rev_v_i;
                 current_sum_ascending += v_i;
             });
 
-        out.iter_mut().for_each(|x| *x /= self.len as f64);
+        out.iter_mut().for_each(|x| *x /= self.len() as f64);
 
         out
     }
@@ -76,22 +78,22 @@ impl<'a> GrandMeans<'a> {
     /// The resulting grand means are written into `self.out`.
     /// The algorithm has complexity `O(v.len())`.
     pub fn compute_unordered(self, order: &[usize]) -> Vec<f64> {
-        assert_eq!(order.len(), self.len, "order must be same length as v");
+        assert_eq!(order.len(), self.len(), "order must be same length as v");
         let mut current_sum_ascending = 0.0;
         let mut current_sum_descending = 0.0;
-        let mut out = vec![0.0; self.len];
+        let mut out = vec![0.0; self.len()];
 
         izip!(order.iter(), order.iter().rev())
             .enumerate()
             .for_each(|(i, (ord_j, rev_ord_j))| {
-                out[*ord_j] += (2 * (i as i64) - self.len as i64 + 1) as f64 * self.v[*ord_j]
+                out[*ord_j] += (2 * (i as i64) - self.len() as i64 + 1) as f64 * self.v[*ord_j]
                     - current_sum_ascending;
                 out[*rev_ord_j] += current_sum_descending;
                 current_sum_descending += self.v[*rev_ord_j];
                 current_sum_ascending += self.v[*ord_j];
             });
 
-        out.iter_mut().for_each(|x| *x /= self.len as f64);
+        out.iter_mut().for_each(|x| *x /= self.len() as f64);
 
         out
     }
@@ -109,24 +111,24 @@ impl<'a> GrandMeans<'a> {
     /// The resulting grand means are written into `self.out`.
     /// The algorithm has complexity `O(v.len())`.
     pub fn compute_ordered_weighted(&mut self, w: &[f64]) -> Vec<f64> {
-        assert_eq!(w.len(), self.len, "weights must be same length as v");
+        assert_eq!(w.len(), self.len(), "weights must be same length as v");
         let mut current_sum_ascending = 0.0;
         let mut current_sum_descending = 0.0;
-        let mut out = vec![0.0; self.len];
+        let mut out = vec![0.0; self.len()];
 
         let mut sum = -w[1..].iter().sum::<f64>();
-        for i in 0..self.len {
+        for i in 0..self.len() {
             out[i] += sum * self.v[i] - current_sum_ascending;
-            if i < self.len - 1 {
+            if i < self.len() - 1 {
                 sum += w[i] + w[i + 1];
             }
-            let rev_ord_i = self.len - i - 1;
+            let rev_ord_i = self.len() - i - 1;
             out[rev_ord_i] += current_sum_descending;
             current_sum_descending += self.v[rev_ord_i] * w[rev_ord_i];
             current_sum_ascending += self.v[i] * w[i];
         }
 
-        out.iter_mut().for_each(|x| *x /= self.len as f64);
+        out.iter_mut().for_each(|x| *x /= self.len() as f64);
 
         out
     }
